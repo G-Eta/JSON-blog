0102bd3068fd612ee1a4e1e3a9782fe0ed76d723
G-Eta <104272243+G-Eta@users.noreply.github.com>
2022-06-19 13:05:23 +0100

    Add files via upload

--- js/view.js
+++ js/view.js
@@ -1,30 +1,4 @@
-/* const postList = document.querySelector('#posts');
-const url = 'https://jsonplaceholder.typicode.com/posts';
 
-postBox = [];
-
-const renderPosts = (dataArr) => {
-    output = '';
-  dataArr.forEach(post => {
-    postHolder += `
-        <div class="col-md-4 mb-3">
-            <div class="card h-100 bg-secondary">
-             <div class="card-body text-white bg-secondary">
-                <p class="text-center">${post.id}</p>
-                <h6 class="post-title text-uppercase fw-bold text-center">${post.title}</h6>
-                <p class="post-body">${post.body}</p>
-                </div>
-             <div class="d-flex justify-content-between mx-2 my-2">
-                <button class="btn btn-dark" id="view-btn" onclick="openSingle(${post.id})">View</button>
-                    <button class="btn btn-dark" onclick="updatePost(${post.id})">Update</button>
-                    <button class="btn btn-dark" onclick="deletePost(${post.id})">Delete</button>
-                </div>
-            </div>
-        </div>`
-  });
-  
-  postList.innerHTML = output;
-} */
 
 
 
