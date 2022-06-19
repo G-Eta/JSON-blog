82398c3ddb6e285d3d15c0910e73b7919a00ffef
G-Eta <104272243+G-Eta@users.noreply.github.com>
2022-06-19 18:50:51 +0100

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,13 +1,7 @@
 
-
-
-
-
 function viewSingle() {
     let mainBlog = localStorage.getItem('viewedPost')
-    console.log(mainBlog);
     let post = JSON.parse(mainBlog)
-    console.log(post)
     document.getElementById('post-id').innerHTML = post.id
     document.getElementById('post-title').innerHTML = post.title
     document.getElementById('post-body').innerHTML = post.body
